5523450422b4df74f7416a5b042ef5ce2a2e54d5
Jarosław Sobczak <jareksobczak9@gmail.com>
2019-07-04 13:36:16 +0200

    przykładowe implementacje interface Function

--- src/main/java/com/ownProjects/lambdas/function/FunctionUse.java
+++ src/main/java/com/ownProjects/lambdas/function/FunctionUse.java
@@ -1,6 +1,8 @@
 package com.ownProjects.lambdas.function;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 
 public class FunctionUse {
@@ -15,8 +17,20 @@ public class FunctionUse {
         LocalDate date = textToDate.apply("2001-01-20");
         System.out.println(date);
         //lub jako lambda:
-        Function<String,LocalDate>textToDateLambda=text->LocalDate.parse(text);
-        LocalDate dateLambda=textToDateLambda.apply("1987-11-21");
+        Function<String, LocalDate> textToDateLambda = text -> LocalDate.parse(text);
+        LocalDate dateLambda = textToDateLambda.apply("1987-11-21");
         System.out.println(dateLambda);
+
+        //zamiana Stringa na Integera ..
+
+        Function<String, Integer> stringIntegerFunction = text -> Integer.valueOf(text);
+        Integer value = stringIntegerFunction.apply("1234");
+        System.out.println(value);
+
+        //zamiana listy stringów na jej pierwszy element ..
+        Function<List<String>, String> getFirstWord = list -> {
+            return list.isEmpty() ? null : list.get(0);
+        };
+        System.out.println(getFirstWord.apply(Arrays.asList("jeden", "dwa", "trzy")));
     }
 }
